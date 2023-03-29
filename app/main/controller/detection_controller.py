f89205a8c844c3ca05d24c8a3f979287bedfb449
neburpoots <neburpoots@hotmail.com>
2023-03-29 17:05:11 +0200

    added ultralytics to requirements.txt and added model to project

---
+++ app/main/controller/detection_controller.py
@@ -0,0 +1,25 @@
+from flask import request
+from flask_restx import Resource
+
+from ..util.dto import UserDto
+from ..service.user_service import get_a_user
+
+api = UserDto.api
+_user = UserDto.user
+
+@api.route('/<public_id>')
+@api.param('public_id', 'The User identifier')
+@api.response(404, 'User not found.')
+class User(Resource):
+    @api.doc('get a user')
+    @api.marshal_with(_user)
+    def get(self, public_id):
+        """get a user given its identifier"""
+        user = get_a_user(public_id)
+        if not user:
+            api.abort(404)
+        else:
+            return user
+
+
+
