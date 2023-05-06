efa0cfd5d2044b48a2950cefa8fb6c1773684498
neburpoots <neburpoots@hotmail.com>
2023-05-06 20:02:54 +0200

    max content length increased even more

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
 app.register_blueprint(blueprint)
 
 #changes max file upload size to 100mb
-app.config['MAX_CONTENT_LENGTH'] = 100 * 1000 * 1000
+app.config['MAX_CONTENT_LENGTH'] = 1000 * 1000 * 1000
 
 app.app_context().push()
 
